5cce5b185b263824532d129314bbfc9243058310
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2022-11-23 01:36:14 +0500

    Adding Material UI

---
+++ frontend2/frontend/src/components/Course/addcourse.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+
+import "./addcourse.css";
+
+const AddCourse = () => {
+  return (
+    <div style={{}}>
+      <Card
+        style={{
+          width: 400,
+          backgroundColor: "white",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h5" component="h2" color="textSecondary">
+            Object Oriented Programing
+          </Typography>
+
+          <Typography variant="body2" component="p" color="textSecondary">
+            BS-Sec A Fall 2022
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" style={{ backgroundColor: "#880e4f" }}>
+            Open Classroom
+          </Button>
+
+          <Button
+            variant="contained"
+            href="#contained-buttons"
+            style={{ backgroundColor: "#b71c1c" }}
+          >
+            Remove
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default AddCourse;

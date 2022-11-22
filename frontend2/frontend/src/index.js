3c5da85557f3ac08f728422f0daf4b571f55221e
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2022-11-22 23:55:45 +0500

    Basic Config 1

---
+++ frontend2/frontend/src/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>Hello, Welcome to React and TypeScript</h1>
+  </div>,
+  document.getElementById("root")
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

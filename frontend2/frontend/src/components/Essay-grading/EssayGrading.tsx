17611dcac30c0204c16fd57f194cb1b7c48db28b
Bilal Ahmed <58539283+bilalahmed15@users.noreply.github.com>
2022-11-28 22:04:14 -0800

    Update EssayGrading.tsx

--- frontend2/frontend/src/components/Essay-grading/EssayGrading.tsx
+++ frontend2/frontend/src/components/Essay-grading/EssayGrading.tsx
@@ -1,27 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import * as Icon from 'react-bootstrap-icons';
 
 function EssayGrading() {
   return (
     <>
+    <div style={{marginLeft:"100px",marginTop:"50px", color:"#4e2a84"}}>
+
+<Button variant="outline-secondary" style={{color:"white",backgroundColor:"#4e2a84"}}><Icon.ArrowLeft size={'20'}/>Back</Button>
+</div>
     <div>
-        <h1 style={{marginLeft: '150px',padding:"50px",color:"#880e4f"}}>Essay Grading</h1>
+        <h1 style={{marginLeft: '150px',padding:"50px",color:"#4e2a84"}}>Essay Grading</h1>
     </div>
-    <Form style={{marginLeft: '200px',marginRight:'70px'}}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Input Essay</Form.Label>
-        <Form.Control type="essay" placeholder="Input Essay"  style={{height:"200px"}} />
+    <Form style={{marginLeft: '200px',marginRight:'70px',backgroundColor:"#e4e0ee",borderRadius:"13px"}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail" style={{color:"#4e2a84",padding:"40px"}}>
+        <Form.Label className="fw-bold">Input Essay</Form.Label>
+        <Form.Control type="essay" style={{height:"200px"}} />
        
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Input Essay Answer</Form.Label>
-        <Form.Control type="essay-ans" placeholder="Input Essay Answer" style={{height:"200px"}}/>
+      <Form.Group className="mb-3" controlId="formBasicEmail" style={{color:"#4e2a84",padding:"40px"}}>
+        <Form.Label className="fw-bold">Input Essay Answer</Form.Label>
+        <Form.Control type="essay-ans"  style={{height:"200px"}}/>
        
       </Form.Group>
-      <Button variant="primary" type="submit" style={{backgroundColor:"#880e4f",marginLeft:"250px",width:"700px"}}>
-        Submit
-      </Button>
+      <Button variant="outline-light" className="fw-bold" style={{color:"white",backgroundColor:"#4e2a84",marginLeft:"250px",width:"700px"}}>Submit</Button>{' '}
+      
+      <br/><br/><br/>
     </Form>
     </>
   );

4b55c909526f987abccae9d1251a9277a67bda70
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2022-12-03 05:49:03 +0500

    Update QuestionAnswer.tsx

--- frontend2/frontend/src/components/QuestionAnswer/QuestionAnswer.tsx
+++ frontend2/frontend/src/components/QuestionAnswer/QuestionAnswer.tsx
@@ -4,7 +4,51 @@ import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 import Button from "react-bootstrap/Button";
 import * as Icon from "react-bootstrap-icons";
+import axios from "axios";
+
+import React, { useState, useEffect } from "react";
+
 function QuestionAnswer() {
+  const [answer, setAnswer] = useState(null);
+
+  const [contextState, setContextState] = useState("");
+  const [questionState, setQuestionState] = useState("");
+
+  const getAnswer = (context: string, question: string) => {
+    const data = {
+      context: context,
+      question: question,
+    };
+    const getAnswer = async () => {
+      try {
+        const res = await axios.post("http://127.0.0.1:8000/api/model/", data);
+        setAnswer(res.data["answer"]);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getAnswer();
+    console.log(answer);
+
+    return answer;
+  };
+
+  const onSubmit = () => {
+    getAnswer(contextState, questionState);
+  };
+
+  const onContextChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setContextState(event.target.value);
+  };
+
+  const onQuestionChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setQuestionState(event.target.value);
+  };
+
   return (
     <>
       <div style={{ marginLeft: "50px", marginTop: "50px", color: "#4e2a84" }}>
@@ -30,7 +74,14 @@ function QuestionAnswer() {
             style={{ color: "#4e2a84" }}
           >
             <Form.Label>Context</Form.Label>
-            <Form.Control as="textarea" placeholder="Context" rows={3} />
+            <Form.Control
+              as="textarea"
+              placeholder="Context"
+              rows={3}
+              id="context"
+              value={contextState}
+              onChange={onContextChangeHandler}
+            />
           </Form.Group>
           <Form.Group
             className="mb-3"
@@ -38,7 +89,13 @@ function QuestionAnswer() {
             style={{ color: "#4e2a84" }}
           >
             <Form.Label>Question</Form.Label>
-            <Form.Control type="question" placeholder="Question?" />
+            <Form.Control
+              type="question"
+              placeholder="Question"
+              id="question"
+              value={questionState}
+              onChange={onQuestionChangeHandler}
+            />
           </Form.Group>
           <Button
             variant="outline-secondary"
@@ -47,6 +104,7 @@ function QuestionAnswer() {
               backgroundColor: "#4e2a84",
               color: "white",
             }}
+            onClick={() => onSubmit()}
           >
             Get Answer
           </Button>{" "}
@@ -56,7 +114,7 @@ function QuestionAnswer() {
             style={{ color: "#4e2a84" }}
           >
             <Form.Label>Answer</Form.Label>
-            <Form.Control type="answer" placeholder="Answer" />
+            <Form.Control type="answer" placeholder={answer}></Form.Control>
           </Form.Group>
         </Form>
       </div>

4e83c9467ed133fed2cbd192b5c0f35e04980877
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2022-12-21 13:50:26 +0500

    Update models.py

--- server/src/dashboard/models.py
+++ server/src/dashboard/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 class Account(models.Model):
-    username = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
+    username = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+# class StudentAccount(Account):
+#     account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.name
+
+# class TeacherAccount(Account):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return self.name
 
 class Course(models.Model):
     title = models.CharField(max_length=120)

e4891ac96dbdfd648f81629e0f83bb844afd8bb5
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2023-01-07 21:48:03 +0500

    "Fixtures + Models "
    This reverts commit a8f071b2d0f0da0149e25c9391ddf27270da2c6f.

--- server/src/dashboard/admin.py
+++ server/src/dashboard/admin.py
@@ -6,10 +6,8 @@ admin.site.register(Course)
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(User)
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'is_teacher')
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'first_name', 'last_name', 'email', 'is_teacher')
-
-# admin.site.register(User, UserAdmin)
+admin.site.register(User, UserAdmin)

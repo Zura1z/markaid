9d5a4b91f64e975afd5b1ef86020773c55c2c7cf
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2023-01-07 22:33:56 +0500

    apiClientFile + QA

--- web/src/components/QuestionAnswer/QuestionAnswer.tsx
+++ web/src/components/QuestionAnswer/QuestionAnswer.tsx
@@ -8,11 +8,18 @@ import axios from "axios";
 
 import React, { useState, useEffect } from "react";
 
+import { QARequestData } from "../../api/types";
+
+import { makeQARequest } from "../../api/apiClient";
+
 function QuestionAnswer() {
   const [answer, setAnswer] = useState(null);
 
-  const [contextState, setContextState] = useState("");
-  const [questionState, setQuestionState] = useState("");
+  const sampleContext =
+    "Barack Obama, in full Barack Hussein Obama II, (born August 4, 1961, Honolulu, Hawaii, U.S.), 44th president of the United States (2009–17) and the first African American to hold the office. Before winning the presidency, Obama represented Illinois in the U.S. Senate (2005–08). He was the third African American to be elected to that body since the end of Reconstruction (1877). In 2009 he was awarded the Nobel Peace Prize “for his extraordinary efforts to strengthen international diplomacy and cooperation between peoples. When Obama was age two, Barack Sr. left to study at Harvard University; shortly thereafter, in 1964, Ann and Barack Sr. divorced. (Obama saw his father only one more time, during a brief visit when Obama was 10.) Later Ann remarried, this time to another foreign student, Lolo Soetoro from Indonesia, with whom she had a second child, Maya. Obama lived for several years in Jakarta with his half sister, mother, and stepfather. While there, Obama attended both a government-run school where he received some instruction in Islam and a Catholic private school where he took part in Christian schooling. He returned to Hawaii in 1971 and lived in a modest apartment, sometimes with his grandparents and sometimes with his mother (she remained for a time in Indonesia, returned to Hawaii, and then went abroad again—partly to pursue work on a Ph.D.—before divorcing Soetoro in 1980). For a brief period his mother was aided by government food stamps, but the family mostly lived a middle-class existence. In 1979 Obama graduated from Punahou School, an elite college preparatory academy in Honolulu.";
+  const [contextState, setContextState] = useState(sampleContext);
+  const sampleQuestion = "Where did Obama study?";
+  const [questionState, setQuestionState] = useState(sampleQuestion);
 
   const getAnswer = (context: string, question: string) => {
     const data = {
@@ -21,8 +28,8 @@ function QuestionAnswer() {
     };
     const getAnswer = async () => {
       try {
-        const res = await axios.post("http://127.0.0.1:8000/api/model/", data);
-        setAnswer(res.data["answer"]);
+        const res = await makeQARequest(data);
+        setAnswer(res["answer"]);
       } catch (e) {
         console.log(e);
       }

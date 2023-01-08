d5ecbd738e021a123d2d6db505efa938b49c8bd9
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2023-01-08 07:54:41 +0500

    Integration of Course ( Backend / Frontend )

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,16 +4,14 @@ import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AssesmentsUI from "./components/Assesments/Assesments";
 
-
-
-
 import OpenClassroom from "./components/Course/StudentClassroom/OpenClasrrom";
 import StudentClassroom from "./components/Course/StudentClassroom/StudentClassroom";
+import TeacherClassroom from "./pages/TeacherClassroom";
 import ExamDetails from "./components/CreateExam/ExamsDetails";
 import Footer from "./components/Footer/Footer";
 
-import { SideBar } from "./components/Sidebar/Sidebar";
-
+// import { SideBar } from "./components/Sidebar/Sidebar";
+import QuestionAnswer from "./components/QuestionAnswer/QuestionAnswer";
 ReactDOM.render(
   <>
     {/* <React.StrictMode>
@@ -22,13 +20,12 @@ ReactDOM.render(
         <SideBar />
       </BrowserRouter>
     </React.StrictMode> */}
-    <SideBar/>
+    {/* <SideBar /> */}
     {/* <AssesmentsUI/> */}
-    <AssesmentsUI/>
+    {/* <AssesmentsUI/> */}
     {/* <Assesment/> */}
-    <Footer/>
-
-    ,
+    {/* <Footer /> */}
+    <TeacherClassroom id={1} />,{/* <QuestionAnswer /> */}
   </>,
   document.getElementById("root")
 );
